e25411614e2467e59fbc0247aa3024f048eb71c7
muhammadsami <muhammadsami1242@gmail.com>
2025-01-03 02:02:34 +0500

    added: reusable component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,23 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        secondary: "var(--secondary)",
+        Neutral: {
+          Primary: "var(--neutral1)",
+          Secondary: "var(--neutral2)",
+        },
+        Primary: {
+          button: "var(--button)",
+          buttonLight: "var(--button-light)",
+          buttonDark: "var(--button-dark)",
+        },
       },
     },
   },

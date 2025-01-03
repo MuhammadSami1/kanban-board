a5bb3151ac294cfe8cf360c46d606fd8cae69842
muhammadsami <muhammadsami1242@gmail.com>
2025-01-03 23:53:03 +0500

    added: new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from "../../components/Button";
 export default function Home() {
   return (
     <div>
-      <Button className="p-5" variant="button" size="lg">
+      <Button className="p-10" variant="button" size="lg">
         Hello
       </Button>
     </div>

5b32d9a6eba1c895522f11523981b4da8c1e143e
muhammadsami <muhammadsami1242@gmail.com>
2025-01-25 20:30:02 +0500

    navbar completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Button from '@/components/ui/Button'
-
 export default function Home() {
-  return (
-    <main>
-      <Button className="p-5" variant="button" size="lg">
-        Click me
-      </Button>
-    </main>
-  )
+  return <main>Main</main>
 }
